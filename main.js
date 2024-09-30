8fac11ccb4653eb838bffa5b99b4a50ab5019ed9
aabaza97 <aabaza32@hotmail.com>
2024-09-30 14:34:14 +0300

    responding to ping command

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const PORT = 6379;
 
 const server = net.createServer((connection) => {
 	console.log('some one connected!');
+
+	// Responding to incoming data
+	connection.on('data', (data) => {
+		if (data.toString().toLocaleLowerCase() === 'ping\r\n') {
+			connection.write('+PONG\r\n');
+		} else {
+			connection.write('Unknown command\r\n');
+		}
+	});
 });
 
 server.listen(PORT, HOSTNAME, () => {
