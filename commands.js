16d1fda7fbe5c1c51f719600615be8354e9f118f
aabaza97 <aabaza32@hotmail.com>
2024-09-30 16:58:23 +0300

    Add support for px expiry flag for the set command

--- commands.js
+++ commands.js
@@ -7,6 +7,14 @@ module.exports.echo = (...args) => `+${args.join(' ')}`;
 module.exports.set = (...args) => {
 	const key = args[0];
 	const value = args[1];
+
+	if (args[2] === 'px' && args[3]) {
+		setTimeout(() => {
+			console.log(`deleting ${key}`);
+			delete memoryLike[key];
+		}, args[3]);
+	}
+
 	memoryLike[key] = value;
 
 	return '+OK';
