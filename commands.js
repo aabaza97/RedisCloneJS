3004fc470cc215d062bef9477237ecf8988bed94
aabaza97 <aabaza32@hotmail.com>
2024-09-30 16:35:17 +0300

    Add support for get and set commands

--- commands.js
+++ commands.js
@@ -1,3 +1,23 @@
+const memoryLike = {};
+
 module.exports.ping = () => '+pong';
 
 module.exports.echo = (...args) => `+${args.join(' ')}`;
+
+module.exports.set = (...args) => {
+	const key = args[0];
+	const value = args[1];
+	memoryLike[key] = value;
+
+	return '+OK';
+};
+
+module.exports.get = (...args) => {
+	const key = args[0];
+
+	if (memoryLike[key]) {
+		return memoryLike[key];
+	}
+
+	return '-1';
+};
